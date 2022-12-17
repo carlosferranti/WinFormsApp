8254235003474724655d550f372e981c097154b6
Carlos Silva <compasso-carlos.silva@ailos.coop.br>
2022-12-17 18:39:34 -0300

    .

--- WinFormsApp/FrmSobre.cs
+++ WinFormsApp/FrmSobre.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
 
 namespace WinFormsApp
 {
@@ -21,6 +22,14 @@ namespace WinFormsApp
         private void btnSair_Click(object sender, EventArgs e)
         {
             this.Close();
+
+            //FrmPrincipal frmPrincipal = new FrmPrincipal();
+            //frmPrincipal.ShowDialog();
+            //frmPrincipal.Show();
+
+            //Form1 _f1;
+            //_f1 = new Form1();
+            //_f1.Show();
         }
 
         private void FrmSobre_FormClosing(object sender, FormClosingEventArgs e)
@@ -29,9 +38,18 @@ namespace WinFormsApp
         }
 
         private void FrmSobre_Load(object sender, EventArgs e)
-        {         
+        {
+            //Version appVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            //lblApp.Text = string.Format("Versão do aplicativo: {0}.{1}.{2} ", appVersion.Major, appVersion.Minor, appVersion.Build);
+
+            lblApp.Text = GetVersionApp();
+        }
+
+        private string GetVersionApp()
+        {
             Version appVersion = Assembly.GetExecutingAssembly().GetName().Version;
-            lblApp.Text = string.Format("Versão do aplicativo: {0}.{1}.{2} ", appVersion.Major, appVersion.Minor, appVersion.Build);
-        }    
+            return string.Format("Versão do aplicativo: {0}.{1}.{2} ", appVersion.Major, appVersion.Minor, appVersion.Build);
+
+        }
     }
 }
