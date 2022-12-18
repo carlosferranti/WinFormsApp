62501c0da73de78db1c9c36aef42f91e2fc49459
Carlos Silva <compasso-carlos.silva@ailos.coop.br>
2022-12-17 22:57:33 -0300

    .

---
+++ WinFormsApp/FrmClientes.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WinFormsApp
+{
+    partial class FrmClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelPrincipal
+            // 
+            this.panelPrincipal.Size = new System.Drawing.Size(800, 405);
+            // 
+            // panel
+            // 
+            this.panel.Size = new System.Drawing.Size(800, 45);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // FrmClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FrmClientes";
+            this.Text = "FrmClientes";
+            this.Load += new System.EventHandler(this.FrmClientes_Load);
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
