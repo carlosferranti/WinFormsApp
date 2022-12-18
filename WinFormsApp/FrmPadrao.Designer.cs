761d155a016aa307192c96265ade16b1b022cd2a
Carlos Silva <compasso-carlos.silva@ailos.coop.br>
2022-12-17 22:15:01 -0300

    .

---
+++ WinFormsApp/FrmPadrao.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WinFormsApp
+{
+    partial class FrmPadrao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.panelPrincipal = new System.Windows.Forms.Panel();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.btnAlterar);
+            this.panel.Controls.Add(this.btnPesquisar);
+            this.panel.Controls.Add(this.btnSair);
+            this.panel.Controls.Add(this.btnCancelar);
+            this.panel.Controls.Add(this.btnExcluir);
+            this.panel.Controls.Add(this.btnSalvar);
+            this.panel.Controls.Add(this.btnNovo);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1178, 75);
+            this.panel.TabIndex = 0;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(306, 20);
+            this.btnAlterar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(107, 38);
+            this.btnAlterar.TabIndex = 6;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(784, 20);
+            this.btnPesquisar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(107, 38);
+            this.btnPesquisar.TabIndex = 5;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(1019, 20);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(107, 38);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(594, 20);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(107, 38);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(450, 20);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(107, 38);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(161, 20);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(107, 38);
+            this.btnSalvar.TabIndex = 1;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(17, 20);
+            this.btnNovo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(107, 38);
+            this.btnNovo.TabIndex = 0;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // panelPrincipal
+            // 
+            this.panelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPrincipal.Location = new System.Drawing.Point(0, 75);
+            this.panelPrincipal.Name = "panelPrincipal";
+            this.panelPrincipal.Size = new System.Drawing.Size(1178, 469);
+            this.panelPrincipal.TabIndex = 1;
+            // 
+            // FrmPadrao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 544);
+            this.Controls.Add(this.panelPrincipal);
+            this.Controls.Add(this.panel);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmPadrao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmPadrao";
+            this.Load += new System.EventHandler(this.FrmPadrao_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmPadrao_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FrmPadrao_KeyPress);
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected Panel panelPrincipal;
+        protected Panel panel;
+        protected Button btnPesquisar;
+        protected Button btnSair;
+        protected Button btnCancelar;
+        protected Button btnExcluir;
+        protected Button btnSalvar;
+        protected Button btnNovo;
+        protected Button btnAlterar;
+    }
+}
---
+++ WinFormsApp/FrmPadrao.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class FrmPadrao : Form
+    {
+        //public enum StatusOperacao
+        //{
+        //    scInsert, scBrowser, scEdit, scCancel
+        //}
+
+        public string StatusOperacao;
+
+
+        public enum sStatus
+        {
+
+        }
+        public sStatus status;
+
+        public FrmPadrao()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPadrao_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void FrmPadrao_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+        }
+
+        private void HabilitaDesabilitaControles(bool Value)
+        {
+            foreach (Control ctl in this.Controls)
+            {
+                if (ctl is Panel)
+                    continue;
+                ctl.Enabled = Value;
+            }
+        }
+
+        private void LimpaControles()
+        {
+            foreach (Control ctl in this.Controls)
+            {
+                if (ctl is TextBox)
+                    (ctl as TextBox).Text = "";
+                if (ctl is ComboBox)
+                    (ctl as ComboBox).SelectedIndex = -1;
+                if (ctl is ListBox)
+                    (ctl as ListBox).SelectedIndex = -1;
+            }
+        }
+
+        private void FrmPadrao_Load(object sender, EventArgs e)
+        {
+            //sStatus = StatusCadastro.scBrowser;
+            //LimpaControles();
+            //HabilitaDesabilitaControles(false);
+        }
+
+        //public virtual bool Salvar()
+        //{
+
+        //}
+
+        //public virtual bool Excluir()
+        //{
+
+        //}
+
+        //public virtual bool Localizar()
+        //{
+
+        //}
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //if (Salvar())
+            //{
+            //    sStatus = StatusCadastro.scBrowser;
+            //    HabilitaDesabilitaControles(false);
+            //    MessageBox.Show("Registro salvo com sucesso!",
+            //    "Salvar", MessageBoxButtons.OK,
+            //    MessageBoxIcon.Information);
+            //}
+        }
+
+        public void AlterarBotoes(int operacao)
+        {
+            // funções de cada botão
+            // 1 - inserir e localizar
+            // 2 - inserir/alterar
+            // 3 - preparar para excluir ou alterar
+
+            panel.Enabled = false;
+            btnNovo.Enabled = false;
+            btnAlterar.Enabled = false;
+            btnPesquisar.Enabled = false;
+            btnExcluir.Enabled = false;
+            //btnCancelar.Enabled = false;
+            btnSalvar.Enabled = false;
+
+            if (operacao == 1)
+            {
+                btnSalvar.Enabled = true;
+                btnPesquisar.Enabled = true;
+            }
+
+            if (operacao == 2)
+            {
+                panel.Enabled = true;
+                btnSalvar.Enabled = true;
+                btnCancelar.Enabled = true;
+            }
+
+            if (operacao == 3)
+            {
+                btnAlterar.Enabled = true;
+                btnExcluir.Enabled = true;
+                btnCancelar.Enabled = true;
+            }
+        }
+    }
+}
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+//namespace WinFormsApp
+//{
+//    public partial class FrmPadrao : Form
+//    {
+//        public enum StatusCadastro
+//        {
+//            scInsert, scBrowser, scEdit, scCancel
+//        }
+
+//        public enum sStatus
+//        {
+
+//        }
+//        public sStatus status;  
+
+//        public FrmPadrao()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void FrmPadrao_KeyPress(object sender, KeyPressEventArgs e)
+//        {
+
+//        }
+
+//        private void FrmPadrao_KeyDown(object sender, KeyEventArgs e)
+//        {
+//            if (e.KeyCode == Keys.Escape)
+//                this.Close();
+//        }
+
+//        private void HabilitaDesabilitaControles(bool Value)
+//        {
+//            foreach (Control ctl in this.Controls)
+//            {
+//                if (ctl is Panel)
+//                    continue;
+//                ctl.Enabled = Value;
+//            }
+//        }
+
+//        private void LimpaControles()
+//        {
+//            foreach (Control ctl in this.Controls)
+//            {
+//                if (ctl is TextBox)
+//                    (ctl as TextBox).Text = "";
+//                if (ctl is ComboBox)
+//                    (ctl as ComboBox).SelectedIndex = -1;
+//                if (ctl is ListBox)
+//                    (ctl as ListBox).SelectedIndex = -1;
+//            }
+//        }
+
+//        private void FrmPadrao_Load(object sender, EventArgs e)
+//        {
+//            sStatus = StatusCadastro.scBrowser;
+//            LimpaControles();
+//            HabilitaDesabilitaControles(false);
+//        }
+
+//        public virtual bool Salvar()
+//        { 
+
+//        }
+
+//        public virtual bool Excluir()
+//        { 
+
+//        }
+
+//        public virtual bool Localizar()
+//        { 
+
+//        }
+
+//        private void btnSalvar_Click(object sender, EventArgs e)
+//        {
+//            if (Salvar())
+//            {
+//                sStatus = StatusCadastro.scBrowser;
+//                HabilitaDesabilitaControles(false);
+//                MessageBox.Show("Registro salvo com sucesso!",
+//                "Salvar", MessageBoxButtons.OK,
+//                MessageBoxIcon.Information);
+//            }
+//        }
+//    }
+//}
